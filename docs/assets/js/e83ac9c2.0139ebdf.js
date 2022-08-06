63c7fbc7969d3d3498f3e3af6f1ec2e5320aaee7
PhantomsXR <phantomxm2021@gmail.com>
2022-08-07 01:59:40 +0800

    fix base url

--- docs/assets/js/e83ac9c2.0139ebdf.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkxrmod_manual=self.webpackChunkxrmod_manual||[]).push([[9954],{36951:e=>{e.exports=JSON.parse('{"permalink":"/xrmod-engine-doc/changelog","page":1,"postsPerPage":20,"totalPages":1,"totalCount":1,"blogDescription":"Keep yourself up-to-date about new features in every release","blogTitle":"XRMOD changelog"}')}}]);
